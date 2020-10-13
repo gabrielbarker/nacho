28462c06767fb0ec0a15223951c899ddd7a1b60b
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-13 14:03:12 +0100

    ADD a repl

---
+++ repl/repl.go
@@ -0,0 +1,30 @@
+// repl/repl.go
+
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/gabrielbarker/nacho/lexer"
+	"github.com/gabrielbarker/nacho/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}

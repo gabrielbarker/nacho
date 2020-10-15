3ad997fbf1320f29c8eb3c5807357eca2dc659f7
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-16 00:55:18 +0100

    MAKE repl parse input

--- repl/repl.go
+++ repl/repl.go
@@ -8,23 +8,38 @@ import (
 	"io"
 
 	"github.com/gabrielbarker/nacho/lexer"
-	"github.com/gabrielbarker/nacho/token"
+	"github.com/gabrielbarker/nacho/parser"
 )
 
-const PROMPT = ">> "
+const PROMPT = "∆∆∆ "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
+
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
